a0d9da93e45cf6f3083d3cd4faaa968eadcd5bd5
WizardOfFaraz <WizardOfFaraz@gmail.com>
2022-07-22 17:44:35 -0400

    basic camera capture

---
+++ capture.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import cv2
+
+vid = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while(True):
+  ret, frame = vid.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+  faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+  import pdb; pdb.set_trace()
+  for (x, y, w, h) in faces:
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+  cv2.imshow('frame', frame)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+vid.release()
+cv2.destroyAllWindows()
