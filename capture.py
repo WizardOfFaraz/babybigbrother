b1e871beb89a2b57db2f5574fa204983624ea51a
WizardOfFaraz <wizardoffaraz@gmail.com>
2022-07-24 13:58:48 -0400

    Process 1 frame every 30 fps.

--- capture.py
+++ capture.py
@@ -18,7 +18,8 @@ flags.DEFINE_integer('cooldown_seconds', 0, 'The number of seconds to cooldown o
 flags.DEFINE_boolean('list_chromecast_devices', False, 'If True, will list all available Chromecast devices that may be used.')
 flags.DEFINE_boolean('display_preview_window', False, 'If True, will display a preview of the camera.')
 flags.DEFINE_string('facial_image_location', None, 'The facial image file location.')
-
+flags.DEFINE_integer('number_of_times_to_upsample', 2,
+                     'The number of times to upsample the frame for processing, the higher the number, the further back the image can be.')
 
 def main(argv):
     if FLAGS.list_chromecast_devices:
@@ -54,32 +55,37 @@ def main(argv):
     start_time = 0
     set_pause = False
     found_face = False
+    # Process 1 frame every 30fps for better performance
+    process_this_frame = 29
 
     # Main loop
     while(True):
         ret, frame = vid.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
 
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
-      
-        face_locations = face_recognition.face_locations(rgb_small_frame, number_of_times_to_upsample=2)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.6)
-            name = "Unknown"
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
+        process_this_frame = process_this_frame + 1
+        if process_this_frame % 30 == 0:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+        
+            face_locations = face_recognition.face_locations(rgb_small_frame, number_of_times_to_upsample=FLAGS.number_of_times_to_upsample)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.6)
+                name = "Unknown"
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
 
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-                start_time = time.time()
-                found_face = True
-                if mc.status.player_state != 'PAUSED':
-                    set_pause = True
-                    mc.pause()          
-            face_names.append(name)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                    start_time = time.time()
+                    found_face = True
+                    if mc.status.player_state != 'PAUSED':
+                        set_pause = True
+                        mc.pause()          
+                face_names.append(name)
+
         if set_pause:
             if time.time() - start_time > FLAGS.cooldown_seconds:
                 mc.play()
